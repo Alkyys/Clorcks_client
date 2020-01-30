c5fa96a9778b7b03a6c253a5048e77260721e667
Samuel <samuelvgnl@gmail.com>
2020-01-30 17:47:23 +0100

    Update Home.vue
    add logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="home">
     <div id="head">
-      Head Head Head Head Head Head Head Head Head Head Head Head Head Head Head Head
-      <div id="logo">Mon logo</div>
+      <div id="logo">
+        <img src="../assets/logo/Logo complet.svg" alt="logo de clorcks" />
+      </div>
+
       <div id="bouton">
-        <div id="bouton_signup">signup</div>
-        <div id="bouton_login">login</div>
+        <div id="bouton_signup">Sign up</div>
+        <div id="bouton_login">Log in</div>
       </div>
     </div>
 
@@ -30,9 +32,34 @@ export default {
 <style  lang="scss" scoped>
 .home {
   display: flex;
-  flex-direction: column
+  flex-direction: column;
 }
 #head {
-  background-color: grey;
+  display: flex;
+  justify-content: space-between;
+  #logo {
+    padding-left: 1em; 
+  }
+  #bouton {
+    display: flex;
+    align-items: center;
+    padding-right: 1em;
+    #bouton_signup {
+      padding: 0.8em 2em;
+      background: #08080e 0% 0% no-repeat padding-box;
+      border-radius: 6px;
+      color: white;
+    }
+    #bouton_login {
+      padding: 0.8em 2em;
+    }
+  }
+}
+
+#title {
+  display: flex;
+  justify-content: center;
+  font-size: 4.5em;
+  padding: 1em 0em;
 }
 </style>
