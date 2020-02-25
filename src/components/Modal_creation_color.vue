61b61bcc0a780e4e9e24261bd68d2dad2d637f0e
Samuel <samuelvgnl@gmail.com>
2020-02-25 01:11:14 +0100

    Update Modal_creation_color.vue
    add RGBToHSL

--- src/components/Modal_creation_color.vue
+++ src/components/Modal_creation_color.vue
@@ -15,6 +15,8 @@
           <option>CMYK</option>
         </select>
         <p v-show="type=='Hexadecimal'">{{rgbToHex()}}</p>
+        <p v-show="type=='RGB'">rgb({{color.red}}, {{color.green}}, {{color.blue}})</p>
+        <p v-show="type=='CMYK'">{{RGBToHSL(color.red,color.green,color.blue)}}</p>
         <img src="../assets/logo/copy.svg" alt />
       </div>
 
@@ -74,6 +76,45 @@ export default {
       if (b.length === 1) b = '0' + b
 
       return `#${r}${g}${b}`
+    },
+    RGBToHSL: function (r, g, b) {
+      // Make r, g, and b fractions of 1
+      r /= 255
+      g /= 255
+      b /= 255
+
+      // Find greatest and smallest channel values
+      let cmin = Math.min(r, g, b)
+      let cmax = Math.max(r, g, b)
+      let delta = cmax - cmin
+      let h = 0
+      let s = 0
+      let l = 0
+      // Calculate hue
+      // No difference
+      if (delta == 0) h = 0
+      // Red is max
+      else if (cmax == r) h = ((g - b) / delta) % 6
+      // Green is max
+      else if (cmax == g) h = (b - r) / delta + 2
+      // Blue is max
+      else h = (r - g) / delta + 4
+
+      h = Math.round(h * 60)
+
+      // Make negative hues positive behind 360°
+      if (h < 0) h += 360
+      // Calculate lightness
+      l = (cmax + cmin) / 2
+
+      // Calculate saturation
+      s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1))
+
+      // Multiply l and s by 100
+      s = +(s * 100).toFixed(1)
+      l = +(l * 100).toFixed(1)
+
+      return 'hsl(' + h + ',' + s + '%,' + l + '%)'
     },
     copyValue: function (copyText) {
       copyText.select()
